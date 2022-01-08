82febac2aa9d13d3a52a6db70fce5b19a2ee3aec
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-08 13:43:50 +0000

    removed hbs files and related codes

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const express = require("express");
-const hbs = require("hbs");
-const wax = require("wax-on");
 require("dotenv").config();
 
 // create an instance of express app
 let app = express();
 
-// set the view engine
-app.set("view engine", "hbs");
-
-// static folder
-app.use(express.static("public"));
-
-// setup wax-on
-wax.on(hbs.handlebars);
-wax.setLayoutPath("./views/layouts");
-
-// enable forms
-app.use(
-  express.urlencoded({
-    extended: false
-  })
-);
 
 async function main() {
   app.get("/", (req,res) => {
-    res.render("main")
+    res.send("Welcome")
   })
 }
 
